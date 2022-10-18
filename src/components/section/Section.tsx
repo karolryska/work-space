69c6ccb8e47a856a910023082a3676e4a8d5bee5
Karol Ryska <karolryska@gmail.com>
2022-10-18 23:46:17 +0200

    feat: initialize basic section component

---
+++ src/components/section/Section.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image, { StaticImageData } from 'next/image';
+import classNames from 'classnames/bind';
+import SectionContent from 'components/sectionContent/SectionContent';
+import styles from './Section.module.scss';
+
+const cn = classNames.bind(styles);
+
+interface Props {
+    title: string;
+    body: React.ReactNode;
+    image: StaticImageData;
+    reverse?: boolean;
+}
+
+const Section = ({ title, body, image, reverse }: Props) => {
+    return (
+        <section className={cn('wrapper', { reverse })}>
+            <SectionContent title={title} body={body} />
+            <div className={cn('image')}>
+                <Image src={image} layout="fill" />
+            </div>
+        </section>
+    );
+};
+
+export default Section;
