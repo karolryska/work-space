a9c94d77bc48776a7a3ceb69907bdbca0a8ea789
Karol Ryska <karolryska@gmail.com>
2022-11-03 20:59:01 +0100

    feat: add layout to app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
+import Layout from 'components/layout/Layout';
+import type { AppProps } from 'next/app';
+import { Color } from 'types/types';
 import '../styles/main.scss';
 
-import type { AppProps } from 'next/app';
+interface CustomPageProps extends AppProps {
+    color: Color;
+}
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+interface CustomAppProps extends AppProps {
+    pageProps: CustomPageProps;
+}
+
+function MyApp({ Component, pageProps }: CustomAppProps) {
+    return (
+        <Layout color={pageProps.color}>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
 export default MyApp;
